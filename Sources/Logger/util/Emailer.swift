77b3cb31a73325f7f733394f6474ecc0f194e240
eonist <30n1st@gmail.com>
2023-03-20 17:39:10 +0100

    Deprecate emailer code

--- Sources/Logger/util/Emailer.swift
+++ Sources/Logger/util/Emailer.swift
@@ -5,7 +5,7 @@ import UIKit
  * - Fixme: ⚠️️ Add for macOS as well? or is it supported as is?
  */
 public class Emailer {
-   public typealias MailInitiator = UIViewController & MFMailComposeViewControllerDelegate
+//   public typealias MailInitiator = UIViewController & MFMailComposeViewController
    /**
     * - Fixme: ⚠️️ Add example
     * - Parameters:
@@ -13,26 +13,27 @@ public class Emailer {
     *   - text: text to mail
     *   - recipient: "address@example.com"
     */
-   public static func sendMail(on viewController: MailInitiator, text: String, recipient: String) -> UIAlertAction {
-      UIAlertAction(title: "Send Email", style: .default) {
-         (_: UIAlertAction) in
-         DispatchQueue.main.async {
-            if MFMailComposeViewController.canSendMail() {
-               let composeVC = MFMailComposeViewController()
-               composeVC.mailComposeDelegate = viewController
-               composeVC.setToRecipients([recipient])
-               composeVC.setSubject("Console Log")
-               composeVC.setMessageBody(text, isHTML: false)
-               viewController.present(composeVC, animated: true, completion: nil)
-            } else {
-               let alert = UIAlertController(title: "Email account required",
-                                             message: "Please configure an email account in Mail",
-                                             preferredStyle: .alert)
-               alert.addAction(UIAlertAction.ok())
-               viewController.present(alert, animated: true, completion: nil)
-            }
-         }
-      }
+   public static func sendMail(on viewController: UIViewController/*MFMailComposeViewController*/, text: String, recipient: String) -> UIAlertAction {
+      fatalError("Out of order")
+//      UIAlertAction(title: "Send Email", style: .default) {
+//         (_: UIAlertAction) in
+//         DispatchQueue.main.async {
+//            if MFMailComposeViewController.canSendMail() {
+//               let composeVC = MFMailComposeViewController()
+//               composeVC.mailComposeDelegate = viewController
+//               composeVC.setToRecipients([recipient])
+//               composeVC.setSubject("Console Log")
+//               composeVC.setMessageBody(text, isHTML: false)
+//               viewController.present(composeVC, animated: true, completion: nil)
+//            } else {
+//               let alert = UIAlertController(title: "Email account required",
+//                                             message: "Please configure an email account in Mail",
+//                                             preferredStyle: .alert)
+//               alert.addAction(UIAlertAction.init(title: "OK", style: .default))
+//               viewController.present(alert, animated: true, completion: nil)
+//            }
+//         }
+//      }
    }
 }
 #endif
